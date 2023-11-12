61f49f31b549e8dd9e35e592bc2832d3720a3e5f
Витёк Ромашов <romas-viktor@yandex.ru>
2023-11-12 08:55:02 +0300

    Добавьте файлы проекта.

---
+++ ClassString/String.cpp
@@ -0,0 +1,61 @@
+﻿#include<iostream>
+#include"String.h"
+
+using namespace std;
+
+char* String::createName(char const* name)
+{
+	cout << "String::createName" << endl;
+
+	char* temp = new char[strlen(name) + 1];
+	strcpy_s(temp, strlen(name) + 1, name);
+	return temp;
+}
+
+//String::String(int leght)
+//{
+//	cout << "Конструктор класса String по умолчанию" << endl;
+//	str = new char[length + 1];
+//	str[0] = '\0';
+//	length = 0;
+//}
+
+String::String(const char* s)
+{
+	cout << "Конструктор класса (const char)" << endl;
+	str = new char[length + 1];
+	int i;
+	for (i = 0; i < length && s[i] != '\0'; i++) str[i] = s[i];
+	str[i] = '\0';
+	length = i - 1;
+}
+
+//String::String(const String& ss)
+//{
+//	cout << "Копировать конструктор класса String" << endl;
+//	str = new char[length + 1];
+//	int i;
+//	for (i = 0; i <= ss.length; i++)
+//	{
+//		str[i] = ss.str[i];
+//	}
+//	str[i] = '\0';
+//	length = ss.length;
+//}
+
+String::String(const String& copy)
+{
+	cout << "Копировать конструктор класса String" << endl;
+	if (this->str != nullptr) {
+		delete[] str;
+	}
+
+	length = strlen(copy.str);
+	this->str = new char[length + 1];
+
+	for (int i = 0; i < length; i++)
+	{
+		this->str[i] = copy.str[i];
+	}
+	this->str[length] = '\0';
+}
---
+++ ClassString/String.h
@@ -0,0 +1,81 @@
+﻿#pragma once
+#include<iostream>
+using namespace std;
+
+class String
+{
+	char* str;
+	int length;
+	char* createName(char const* name);
+public:
+
+	//String(char* str) {
+	//	length = strlen(str);// функция strlen получает количество символов в строке которую мы передаём в объект
+
+	//	// выделяем память для динамического массива где будет храниться наша строка
+	//	// +1 символ так как нужно место в массиве под терминирующий 0
+	//	this->str = new char[length + 1];
+	//	// копируем символы строки в массив нашего класса
+	//	for (int i = 0; i < length; i++)
+	//	{
+	//		this->str[i] = str[i];
+	//	}
+	//	//закрываем строку терминирующим нулём
+	//	this->str[length] = '\0';
+	//}
+
+	//String(const String& copy)
+	//{
+	//	if (this->str != nullptr) {
+	//		delete[] str;
+	//	}
+
+	//	length = strlen(copy.str);
+	//	this->str = new char[length + 1];
+
+	//	for (int i = 0; i < length; i++)
+	//	{
+	//		this->str[i] = copy.str[i];
+	//	}
+	//	this->str[length] = '\0';
+
+	//}
+
+
+	~String()
+	{
+		cout << "Деструктор строкового класса" << endl;
+		delete[] this->str;
+	}
+
+
+	//String(int leght);
+	String(const char*);
+	////String(const char);		//?конструктор, принимающий в качестве параметра один символ.
+	String(const String& copy);	//конструктор копирования
+
+	//~String()
+	//{
+	//	cout<< "Деструктор строкового класса" << endl;
+	//	delete[] str;
+	//}
+
+	String& setString(const char* string)
+	{
+		cout << "setString" << endl;
+		delete[] this->str;
+		this->str = createName(string);
+		return *this;
+	}
+
+	//char getString()
+	//{
+	//	return *str;
+	//}
+
+	void show()
+	{
+		cout << "str: " << str << "\n*str " << *str
+			<< "\n&str " << &str << "\nlength " << length << endl;
+	}
+};
